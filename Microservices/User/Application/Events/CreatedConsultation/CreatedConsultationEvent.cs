1cd594a2ce51410168ec8962957ce0cd83e3fac3
Duc Nguyen <anhducmata@gmail.com>
2020-06-14 20:44:49 +0700

    Restructure

---
+++ Microservices/User/Application/Events/CreatedConsultation/CreatedConsultationEvent.cs
@@ -0,0 +1,13 @@
+﻿using Rabbit.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.Application.Events.CreatedConsultation
+{
+    public class CreatedConsultationEvent : BaseEvent
+    {
+        public string Description { get; set; }
+    }
+}
---
+++ Microservices/User/Controllers/PetController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Cqrs;
+using Microsoft.AspNetCore.Mvc;
+
+using User.Application.Commands.CreatePet;
+
+
+namespace User.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        private readonly IBaseCqrs _cqrs;
+
+        public PetController(IBaseCqrs crqs)
+        {
+            this._cqrs = crqs;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            var command = new CreatedPetCommand
+            {
+                Name = "Meo Con"
+            };
+            await this._cqrs.SendCommand(command);
+
+            return Ok();
+        }
+        
+        //[HttpGet]
+        //public async Task<IActionResult> GetAll()
+        //{
+        //    return Ok(await this._petRepository.GetAll()) ;
+        //}
+    }
+}
